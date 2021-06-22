1dfbbffc1d695e22a2b8ea960c5c739e457789c0
Trevor Dawe <trevor.dawe@dell.com>
2021-06-22 13:47:11 -0400

    Added comments

--- handler/cluster_handler.go
+++ handler/cluster_handler.go
@@ -8,16 +8,19 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
+// K8sClientInterface is an interface for support k8s API operations
 //go:generate mockgen -destination=mocks/k8s_client_interface.go -package=mocks github.com/dell/csm-deployment/handler K8sClientInterface
 type K8sClientInterface interface {
 	DiscoverK8sDetails(data []byte) (string, *bool, *kubernetes.Clientset, error)
 }
 
+// ClusterHandler is the handler for Cluster APIs
 type ClusterHandler struct {
 	clusterStore store.ClusterStoreInterface
 	k8sClient    K8sClientInterface
 }
 
+// NewClusterHandler creates a new ClusterHandler
 func NewClusterHandler(cs store.ClusterStoreInterface, k8sClient K8sClientInterface) *ClusterHandler {
 	return &ClusterHandler{
 		clusterStore: cs,
@@ -25,6 +28,7 @@ func NewClusterHandler(cs store.ClusterStoreInterface, k8sClient K8sClientInterf
 	}
 }
 
+// Register will register all Cluster APIs
 func (h *ClusterHandler) Register(api *echo.Group) {
 	jwtMiddleware := middleware.JWT(utils.JWTSecret)
 
--- store/cluster.go
+++ store/cluster.go
@@ -9,6 +9,7 @@ import (
 	"gorm.io/gorm"
 )
 
+// ClusterStoreInterface is used to define the interface for persisting Clusters
 //go:generate mockgen -destination=mocks/cluster_store_interface.go -package=mocks github.com/dell/csm-deployment/store ClusterStoreInterface
 type ClusterStoreInterface interface {
 	Create(*model.Cluster) error
@@ -19,16 +20,19 @@ type ClusterStoreInterface interface {
 	GetAll() ([]model.Cluster, error)
 }
 
+// ClusterStore is used to operate on the Clusters persistent store
 type ClusterStore struct {
 	db *gorm.DB
 }
 
+// NewClusterStore creates a new ClusterStore
 func NewClusterStore(db *gorm.DB) *ClusterStore {
 	return &ClusterStore{
 		db: db,
 	}
 }
 
+// GetByID returns a cluster with the given ID
 func (us *ClusterStore) GetByID(clusterID uint) (*model.Cluster, error) {
 	var m model.Cluster
 	if err := us.db.Preload(clause.Associations).First(&m, clusterID).Error; err != nil {
@@ -40,6 +44,7 @@ func (us *ClusterStore) GetByID(clusterID uint) (*model.Cluster, error) {
 	return &m, nil
 }
 
+// GetAll returns all clusters
 func (us *ClusterStore) GetAll() ([]model.Cluster, error) {
 	var cs []model.Cluster
 	if err := us.db.Preload(clause.Associations).Find(&cs).Error; err != nil {
@@ -51,18 +56,22 @@ func (us *ClusterStore) GetAll() ([]model.Cluster, error) {
 	return cs, nil
 }
 
+// Create will persist a new cluster in the database
 func (us *ClusterStore) Create(u *model.Cluster) (err error) {
 	return us.db.Create(u).Error
 }
 
+// UpdateClusterDetails will update the ClusterDetails association in the database
 func (us *ClusterStore) UpdateClusterDetails(u *model.Cluster, details *model.ClusterDetails) (err error) {
 	return us.db.Model(&u).Association("ClusterDetails").Append(details)
 }
 
+// Update will update an existing cluster in the database
 func (us *ClusterStore) Update(u *model.Cluster) error {
 	return us.db.Save(u).Error
 }
 
+// Delete will delete an existing cluster from the database
 func (us *ClusterStore) Delete(u *model.Cluster) error {
 	return us.db.Delete(u).Error
 }
