eb12f1e1e9eac277e844efa40925e09a5013c468
Trevor Dawe <trevor.dawe@dell.com>
2021-07-21 08:16:00 -0400

    Use constants for min/max cluster versions

--- prechecks/drivers.go
+++ prechecks/drivers.go
@@ -11,20 +11,31 @@ type Validator interface {
 	Validate() error
 }
 
+const (
+	// K8sMinimumSupportedVersion is the minimum supported version for k8s
+	K8sMinimumSupportedVersion = "1.19"
+	// K8sMaximumSupportedVersion is the maximum supported version for k8s
+	K8sMaximumSupportedVersion = "1.21"
+	// OpenshiftMinimumSupportedVersion is the minimum supported version for openshift
+	OpenshiftMinimumSupportedVersion = "4.6"
+	// OpenshiftMaximumSupportedVersion is the maximum supported version for openshift
+	OpenshiftMaximumSupportedVersion = "4.7"
+)
+
 // GetDriverPrechecks will return a list of prechecks for the specific driver and version
 func GetDriverPrechecks(driverType string, clusterData []byte, clusterNodeDetails string) []Validator {
 	validators := make([]Validator, 0)
 
 	// common prechecks for all drivers
 	validators = append(validators, K8sVersionValidator{
-		MinimumVersion: "1.19",
-		MaximumVersion: "1.21",
+		MinimumVersion: K8sMinimumSupportedVersion,
+		MaximumVersion: K8sMaximumSupportedVersion,
 		ClusterData:    clusterData,
 		K8sClient:      k8s.K8sClient{},
 	})
 	validators = append(validators, OpenshiftVersionValidator{
-		MinimumVersion: "4.6",
-		MaximumVersion: "4.7",
+		MinimumVersion: OpenshiftMinimumSupportedVersion,
+		MaximumVersion: OpenshiftMaximumSupportedVersion,
 		ClusterData:    clusterData,
 		K8sClient:      k8s.K8sClient{},
 	})
