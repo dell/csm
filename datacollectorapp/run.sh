438fcdff65334a6cf344c4c1528485217c71abe5
Trevor Dawe <trevor.dawe@dell.com>
2021-09-13 13:12:44 -0400

    Fix NFS precheck for SLES hosts

--- datacollectorapp/run.sh
+++ datacollectorapp/run.sh
@@ -4,6 +4,6 @@ fi
 if [ -f /hostproc/modules ]; then
   echo "sdc=`grep "scini" /hostproc/modules | awk -F  " " '{print $2}'`"
 fi
-if [ -f /hostusr/local/sbin/mount.nfs ] || [ -f /hostusr/local/bin/mount.nfs ] || [ -f /hostusr/sbin/mount.nfs ] || [ -f /hostusr/bin/mount.nfs ]; then
+if [ -f /hostusr/local/sbin/mount.nfs ] || [ -f /hostusr/local/bin/mount.nfs ] || [ -f /hostusr/sbin/mount.nfs ] || [ -f /hostusr/bin/mount.nfs ] || [ -f /hostusr/sbin/rpc.nfsd ]; then
   echo "nfs=true"
 fi
