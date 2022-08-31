eb67c1b51628d5015a397d730e6f064bcc6acb63
Trevor Dawe <trevor.dawe@dell.com>
2022-08-31 15:52:21 -0300

    Remove CSM Common Installer codebase (#458)

--- datacollectorapp/run.sh
+++
@@ -1,9 +0,0 @@
-if [ -f /hostetc/iscsi/initiatorname.iscsi ]; then
-  echo "iscsi=`grep "InitiatorName=" /hostetc/iscsi/initiatorname.iscsi | awk -F  "=" '{print $2}'`"
-fi
-if [ -f /hostproc/modules ]; then
-  echo "sdc=`grep "scini" /hostproc/modules | awk -F  " " '{print $2}'`"
-fi
-if [ -f /hostusr/local/sbin/mount.nfs ] || [ -f /hostusr/local/bin/mount.nfs ] || [ -f /hostusr/sbin/mount.nfs ] || [ -f /hostusr/bin/mount.nfs ] || [ -f /hostusr/sbin/rpc.nfsd ]; then
-  echo "nfs=true"
-fi
