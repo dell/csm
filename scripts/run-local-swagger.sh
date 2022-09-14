1168ffb06d1d663ec0fa383adf6e9091630072c2
sharmilarama <72404078+sharmilarama@users.noreply.github.com>
2022-09-14 11:05:10 -0400

    Rebase 1.4 release branch with changes from main (#468)

--- scripts/run-local-swagger.sh
+++
@@ -1,6 +0,0 @@
-
-# Download swagger.json and replace host with correct IP
-IP=<ip-address-here>:31313 && curl --insecure https://$IP/swagger/doc.json > tmp.json && jq ".host=\"$IP\"" tmp.json > swagger.json && rm tmp.json
-
-# Serve UI with container
-docker run --network=host -e BASE_URL=/swagger -e SWAGGER_JSON=/swagger/swagger.json -v $(pwd):/swagger swaggerapi/swagger-ui
