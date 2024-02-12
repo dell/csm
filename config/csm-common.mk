fb7556834a3a442f7ddac1f6cc24fc9341f3753b
panigs7 <spandita.panigrahi@dell.com>
2024-02-12 01:32:44 -0500

    update ubi micro image

--- config/csm-common.mk
+++ config/csm-common.mk
@@ -1,4 +1,4 @@
 # Common base image for all CSM images. When base image is upgraded, update the following 3 lines with URL, Version, and DEFAULT_BASEIMAGE variable.
-# URL: https://catalog.redhat.com/software/containers/ubi9/ubi-micro/615bdf943f6014fa45ae1b58?architecture=amd64&image=65425acee668a4a85da08f2e
-# Version: ubi9/ubi-micro 9.3-6
-DEFAULT_BASEIMAGE="registry.access.redhat.com/ubi9/ubi-micro@sha256:b7d8fd2840e92e8adc68414e13d859763ef33c3d4c4e27f910e939c00d642c29"
+# URL: https://catalog.redhat.com/software/containers/ubi9/ubi-micro/615bdf943f6014fa45ae1b58?architecture=amd64&image=65a8f97db7e4bede96526c22
+# Version: ubi9/ubi-micro 9.3-13
+DEFAULT_BASEIMAGE="registry.access.redhat.com/ubi9/ubi-micro@sha256:d72202acf3073b61cb407e86395935b7bac5b93b16071d2b40b9fb485db2135d"
