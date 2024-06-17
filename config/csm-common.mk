c323c597a79cd6b924d94cf33a627a25492a4fcc
gallacher <35462391+gallacher@users.noreply.github.com>
2024-06-17 15:33:16 -0300

    Updated registry/catalog location

--- config/csm-common.mk
+++ config/csm-common.mk
@@ -1,5 +1,5 @@
 # Common base image for all CSM images. When base image is upgraded, update the following 3 lines with URL, Version, and DEFAULT_BASEIMAGE variable.
-# URL: https://catalog.redhat.com/software/containers/ubi9/ubi-micro/615bdf943f6014fa45ae1b58?architecture=amd64&image=65a8f97db7e4bede96526c22
+# URL: https://catalog.redhat.com/software/containers/ubi9/ubi-micro/615bdf943f6014fa45ae1b58?architecture=amd64&image=6661257c92bac277d1f762b2
 # Version: ubi9/ubi-micro 9.4-9
-DEFAULT_BASEIMAGE="registry.redhat.io/ubi9/ubi-micro@sha256:979f04176cf3be5e0e70e246fd89184d2bcdde7762cf0b349e85dee5bc4cbb1f"
+DEFAULT_BASEIMAGE="registry.access.redhat.com/ubi9/ubi-micro@sha256:2044e2ca8e258d00332f40532db9f55fb3d0bfd77ecc84c4aa4c1b7af3626ffb"
 DEFAULT_GOIMAGE="golang:1.22"
