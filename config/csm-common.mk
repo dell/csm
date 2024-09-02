b33008853685b7d6a2a7edc4d7465bd7ebd29e1d
csmbot <csm.maintainers@dell.com>
2024-09-02 08:57:36 -0400

    Update UBI Image to ubi9/ubi-micro 9.4-13

--- config/csm-common.mk
+++ config/csm-common.mk
@@ -1,5 +1,5 @@
-# Common base image for all CSM images. When base image is upgraded, update the following 3 lines with URL, Version, and DEFAULT_BASEIMAGE variable.
-# URL: https://catalog.redhat.com/software/containers/ubi9/ubi-micro/615bdf943f6014fa45ae1b58?architecture=amd64&image=6661257c92bac277d1f762b2
-# Version: ubi9/ubi-micro 9.4-9
-DEFAULT_BASEIMAGE="registry.access.redhat.com/ubi9/ubi-micro@sha256:2044e2ca8e258d00332f40532db9f55fb3d0bfd77ecc84c4aa4c1b7af3626ffb"
+# Common base image for all CSM images. When base image is upgraded, do not update anything as it is automated.
+# URL: https://catalog.redhat.com/software/containers/ubi9/ubi-micro/615bdf943f6014fa45ae1b58?architecture=amd64
+# Version: ubi9/ubi-micro 9.4-13
+DEFAULT_BASEIMAGE="registry.access.redhat.com/ubi9/ubi-micro@sha256:9dbba858e5c8821fbe1a36c376ba23b83ba00f100126f2073baa32df2c8e183a"
 DEFAULT_GOIMAGE="golang:1.22"
