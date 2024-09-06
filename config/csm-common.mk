432f982206a6c0a28a6266df1da622a5cd200922
Santhosh Lakshmanan <111809751+santhoshatdell@users.noreply.github.com>
2024-09-06 11:16:26 -0500

    Update go version to 1.23

--- config/csm-common.mk
+++ config/csm-common.mk
@@ -2,4 +2,4 @@
 # URL: https://catalog.redhat.com/software/containers/ubi9/ubi-micro/615bdf943f6014fa45ae1b58?architecture=amd64
 # Version: ubi9/ubi-micro 9.4-13
 DEFAULT_BASEIMAGE="registry.access.redhat.com/ubi9/ubi-micro@sha256:9dbba858e5c8821fbe1a36c376ba23b83ba00f100126f2073baa32df2c8e183a"
-DEFAULT_GOIMAGE="golang:1.22"
+DEFAULT_GOIMAGE="golang:1.23"
