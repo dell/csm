7328faccb0b57d88dcc311421b775ed412d2f6a7
Eric Young <eric.young@emc.com>
2024-09-13 16:16:21 +0000

    Improving csm-base-image
    Adititions to csm-common.mk to allow for easier migration to new base image

--- config/csm-common.mk
+++ config/csm-common.mk
@@ -13,8 +13,23 @@
 # Common settings for all CSM components amd images.
 # Update this file with the image versions change, and it will be automaticall rolled out across all components.
 
+# --- CSMBASEIMAGE settings: Define the values for a common UBI-micro based image 
+CSMBASEIMAGE_REGISTRY="docker.io"
+CSMBASEIMAGE_NAMESPACE="dellemc"
+CSMBASEIMAGE_NAME="csm-base-image"
+CSMBASEIMAGE_TAG_NEWEST=$(curl -s "https://hub.docker.com/v2/namespaces/${CSMBASEIMAGE_NAMESPACE}/repositories/${CSMBASEIMAGE_NAME}/tags?page_size=1000" | grep -o '"name": *"[^"]*' | grep -o '[^"]*$' | grep -E '^([0-9]|v[0-9])' | sort -V | tail -n 1)
+CSMBASEIMAGE_IMAGE="${CSMBASEIMAGE_REGISTRY}/${CSMBASEIMAGE_NAMESPACE}/${CSMBASEIMAGE_NAME}"
+
+# set the CSMBASEIMAGE_TAG_NEWEST if no semanticly versioned tags were found
+ifeq ($(CSMBASEIMAGE_TAG_NEWEST),)
+export CSMBASEIMAGE_TAG_NEWEST="nightly"
+endif
+
+# UBI_BASEIMAGE settings: Define values for the UBI-micro image to be used as a base
+UBI_BASEIMAGE="registry.access.redhat.com/ubi9/ubi-micro@sha256:9dbba858e5c8821fbe1a36c376ba23b83ba00f100126f2073baa32df2c8e183a"
+
 # --- DEFAULT_BASEIMAGE: Specifies the UBI-micro image that is used as the base for the CSM images
-DEFAULT_BASEIMAGE="registry.access.redhat.com/ubi9/ubi-micro@sha256:9dbba858e5c8821fbe1a36c376ba23b83ba00f100126f2073baa32df2c8e183a"
+DEFAULT_BASEIMAGE="${CSMBASEIMAGE_IMAGE}:${CSMBASEIMAGE_TAG_NEWEST}"
 
 # --- DEFAULT_GOVERSION: Specifies the default version of go
 DEFAULT_GOVERSION="1.23"
