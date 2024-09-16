0b6a9a7a70b270c09e4b29ef2c4e5fb21eb63bad
Eric Young <eric.young@emc.com>
2024-09-16 07:31:18 -0500

    Spelling fixes and documentation

--- config/csm-common.mk
+++ config/csm-common.mk
@@ -10,14 +10,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Common settings for all CSM components amd images.
-# Update this file with the image versions change, and it will be automaticall rolled out across all components.
+# This file is intended to be included from within a Makefile,
+# so is restricted to Makefile syntax
 
-# --- CSMBASEIMAGE settings: Define the values for a common UBI-micro based image 
+# Common settings for all CSM components and images.
+# Update this file when the image versions change, and it will be automatically rolled out across all components.
+
+# --- CSMBASEIMAGE settings: Define the values for a common UBI-micro based image
+# registry
 CSMBASEIMAGE_REGISTRY="docker.io"
+# organization
 CSMBASEIMAGE_NAMESPACE="dellemc"
+# iamge
 CSMBASEIMAGE_NAME="csm-base-image"
+# finds the highest semanticly versioned tag
 CSMBASEIMAGE_TAG_NEWEST=$(curl -s "https://hub.docker.com/v2/namespaces/${CSMBASEIMAGE_NAMESPACE}/repositories/${CSMBASEIMAGE_NAME}/tags?page_size=1000" | grep -o '"name": *"[^"]*' | grep -o '[^"]*$' | grep -E '^([0-9]|v[0-9])' | sort -V | tail -n 1)
+# full image name, without tag
 CSMBASEIMAGE_IMAGE="${CSMBASEIMAGE_REGISTRY}/${CSMBASEIMAGE_NAMESPACE}/${CSMBASEIMAGE_NAME}"
 
 # set the CSMBASEIMAGE_TAG_NEWEST if no semanticly versioned tags were found
@@ -25,7 +33,7 @@ ifeq ($(CSMBASEIMAGE_TAG_NEWEST),)
 export CSMBASEIMAGE_TAG_NEWEST="nightly"
 endif
 
-# UBI_BASEIMAGE settings: Define values for the UBI-micro image to be used as a base
+# --- UBI_BASEIMAGE: Value of the UBI-micro image to be used as a base
 UBI_BASEIMAGE="registry.access.redhat.com/ubi9/ubi-micro@sha256:9dbba858e5c8821fbe1a36c376ba23b83ba00f100126f2073baa32df2c8e183a"
 
 # --- DEFAULT_BASEIMAGE: Specifies the UBI-micro image that is used as the base for the CSM images
